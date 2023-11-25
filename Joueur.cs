a37425daf3b4f897f288bc64a5e5fa67ac61e008
Clément <clement.garrigou@edu.devinci.fr>
2023-11-25 22:47:48 +0100

    added some properties

--- Joueur.cs
+++ Joueur.cs
@@ -1,4 +1,12 @@
 class Joueur {
     string nom;
+    List<string> motsTrouves;
+    int score;
+
+    public Joueur(string nom) {
+        this.nom = nom;
+        this.score = 0;
+        this.motsTrouves = new List<string>();
+    }
     
 }
