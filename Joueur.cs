e0905ed76194627f16d69eb5d60372d5101f822a
Alban-gtr <alban.gautier@edu.devinci.fr>
2023-11-29 17:38:19 +0100

    création classe Joueur

--- Joueur.cs
+++ Joueur.cs
@@ -5,8 +5,54 @@ class Joueur {
 
     public Joueur(string nom) {
         this.nom = nom;
-        this.score = 1;
+        this.score = 0;
         this.motsTrouves = new List<string>();
     }
-    
+
+    public string Nom{
+        get{return this.nom;}
+        set{this.nom=value;}
+    }
+    public int Score {
+        get{return this.score;}
+        set{this.score=value;}
+    }
+    public List<string> MotsTrouves {
+        get {return this.motsTrouves;}
+        set {this.motsTrouves=value;}
+    }
+
+    public string toString()
+    {
+        string s="";
+        foreach (string element in motsTrouves)
+        {
+            s+=" "+element;
+        }
+        return ("Voici le joueur 1 : "+nom+". Son score est de "+score+". La liste de mots trouvés est : "+s);
+    }
+
+    public void Add_Mot(string mot)
+    {
+        motsTrouves.Add(mot);
+    }
+
+    public void Add_Score(int val)
+    {
+        score+=val;
+    }
+
+    public bool Contient(string mot)
+    {
+        bool contient = true;
+        foreach(string element in motsTrouves)
+        {
+            if (element==mot)
+                contient=true;
+            else if (element!=mot)
+                contient=false;
+        }
+        return contient;
+    }
 }
+    
