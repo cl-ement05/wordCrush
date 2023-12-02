f75edf62f2cd570d70bb6a413e86c631512be7c5
Alban-gtr <alban.gautier@edu.devinci.fr>
2023-12-02 19:38:35 +0100

    methode toString de la classe Dictionnaire (ne fonctionne pas)

--- Dictionnaire.cs
+++ Dictionnaire.cs
@@ -1,4 +1,53 @@
 class Dictionnaire {
 
+    private string langage;
+    private Dictionary<char, int> motParLettre;
+
+    public Dictionnaire(string langage, string filePath)
+    {
+        this.langage=langage;
+        this.motParLettre=ReadWordsFromFile(filePath);
+    }
+
+    private Dictionary<char, int> ReadWordsFromFile(string filePath)
+    {
+        Dictionary<char, int> motParLettre = new Dictionary<char, int>();
+        try
+        {
+            string[] lines = File.ReadAllLines(filePath);
+            foreach (var line in lines)
+            {
+                foreach (char lettre in line.ToUpper()) // Convertir en majuscules car plus simple
+                {
+                    if (Char.IsLetter(lettre))
+                    {
+                        if (motParLettre.ContainsKey(lettre))
+                        {
+                            motParLettre[lettre]++;
+                        }
+                        else
+                        {
+                            motParLettre[lettre] = 1;
+                        }
+                    }
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Une erreur s'est produite lors de la lecture du fichier.");
+        }
+
+        return motParLettre;
+    }
     
+    public string toString()
+    {
+        string s="Langage : "+langage+"\nNombre de mots par lettre :\n";
+        foreach(var a in motParLettre)
+        {
+            s="{a.Key} : {a.Value}";
+        }
+        return s;
+    }
 }
--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
 ﻿using System;
 
+class Program
+{
+    static void Main()
+    {
+        string filePath="Mots_Français.txt";
+        string langage="Français";
+
+        Dictionnaire dico=new Dictionnaire(langage, filePath);
+        string s=dico.toString();
+        Console.WriteLine(s);
+    }
+}
