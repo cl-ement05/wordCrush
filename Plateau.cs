b8d7dbb14fa8339f799d85c514c497becb45c852
Clément <clement.garrigou@edu.devinci.fr>
2023-12-04 00:55:46 +0100

    first plateau commit, almost done

--- Plateau.cs
+++ Plateau.cs
@@ -1,4 +1,161 @@
 class Plateau {
+    Lettre?[,] tableau;
 
+    public Plateau(Lettre[,] tableau) {
+        this.tableau = tableau;
+    }
+
+    public static Lettre[,] fetchBoardFromFile(string filename, string lettersWeightFile="") {
+        csvInterface fileInterface = new csvInterface(filename, ';');
+        string[,] tableChars = fileInterface.parseFromFile();
+        Lettre[,] board = new Lettre[tableChars.GetLength(0),tableChars.GetLength(1)];
+        Dictionary<char, int> lettersWeight = new Dictionary<char, int>();
+        if (lettersWeightFile != "") {
+            csvInterface fileWeightInterface = new csvInterface(lettersWeightFile, ';');
+            string[,] lettersWeightTable = fileWeightInterface.parseFromFile();
+            for (int i = 0 ; i < lettersWeightTable.GetLength(0); i++) {
+                lettersWeight.Add(char.Parse(lettersWeightTable[i,0]), int.Parse(lettersWeightTable[i,1]));
+            }
+        } else {
+            lettersWeight = new Dictionary<char, int>{
+                ['a'] = 1,
+                ['b'] = 3,
+                ['c'] = 3,
+                ['d'] = 2,
+                ['e'] = 1,
+                ['f'] = 4,
+                ['g'] = 2,
+                ['h'] = 4,
+                ['i'] = 1,
+                ['j'] = 8,
+                ['k'] = 10,
+                ['l'] = 1,
+                ['m'] = 2,
+                ['n'] = 1,
+                ['o'] = 1,
+                ['p'] = 3,
+                ['q'] = 8,
+                ['r'] = 1,
+                ['s'] = 1,
+                ['t'] = 1,
+                ['u'] = 1,
+                ['v'] = 4,
+                ['w'] = 10,
+                ['x'] = 10,
+                ['y'] = 10,
+                ['z'] = 10,
+            };
+        }
+        for (int i = 0; i < tableChars.GetLength(0); i++) {
+            for (int j = 0; j < tableChars.GetLength(1); j++) {
+                board[i,j] = new Lettre(char.Parse(tableChars[i,j]), -1, lettersWeight[char.Parse(tableChars[i,j])]);
+            }
+        }
+        return board;
+    }
+
+    public static Lettre[,] createRandomBoard(string lettersFile, int size=8) {
+        csvInterface fileInterface = new csvInterface(lettersFile, ',');
+        string[,] lettersData = fileInterface.parseFromFile();
+        Lettre[] tabLettres = new Lettre[lettersData.GetLength(0)];
+        for (int i = 0; i < lettersData.GetLength(0); i++) {
+            tabLettres[i] = new Lettre(char.Parse(lettersData[i,0]), int.Parse(lettersData[i,1]), int.Parse(lettersData[i,2]));
+        }
+        Lettre[] probaTable = Lettre.buildProbabilityTable(tabLettres) ?? Array.Empty<Lettre>();
+        Lettre[,] board = new Lettre[0,0];
+        if (probaTable.Length != 0) {
+            board = new Lettre[size, size];
+            Random random = new Random();
+            for (int i = 0; i < board.GetLength(0); i++) {
+                for (int j = 0; j < board.GetLength(1); j++) {
+                    board[i,j] = Lettre.randomLetter(probaTable, random);
+                }
+            }
+        }
+
+        return board;
+    }
+
+    public bool ToFile(string filename) {
+        tableau[3,3] = null;
+        StreamWriter? streamWriter = null;
+        bool success = true;
+        try {
+            streamWriter = new StreamWriter(filename);
+            streamWriter.Write(toString(true));
+        } catch (IOException e) {
+            Console.WriteLine(e.Message);
+            success = false;
+        } catch (Exception e) {
+            Console.WriteLine(e.Message);
+            success = false;
+        }
+        streamWriter?.Close();
+        return success;
+
+    }
     
+    public string toString(bool toCSV = false) {
+        string built = "";
+        for (int i = 0; i < tableau.GetLength(0); i++) {
+            for (int j = 0; j < tableau.GetLength(1); j++) {
+                if (!toCSV) built += tableau[i, j]?.toString() ?? " ";
+                else built += (tableau[i,j]?.toString() ?? "None") + (j != tableau.GetLength(1)-1 ? ";" : "");
+            }
+            built += "\n";
+        }
+        return built;
+    }
+
+    public List<int[]> searchWord(string mot, Joueur joueur) {
+        List<int[]> indexPath = new List<int[]>();
+        if (mot.Length >= 2 && !joueur.MotsTrouves.Contains(mot)) {
+            int indexStart = -1;
+            int lastLine = tableau.GetLength(0)-1;
+            
+            for (int i = 0; i < tableau.GetLength(1); i++) {
+                if (tableau[lastLine,i]!.Character == mot[0]) {
+                    indexStart = i;
+                }
+            }
+            if (indexStart != -1) {
+                return searchWordRecursive(mot, 0, new List<int[]>(), lastLine, indexStart);
+            }
+        }
+        
+        return indexPath;
+    }
+
+    private List<int[]> searchWordRecursive(string mot, int index, List<int[]> indexPath, int i, int j) {
+        if (mot[index] != tableau[i,j]!.Character) return new List<int[]>();
+        else if (index != mot.Length-1) {
+            indexPath.Add(new int[2] {i,j});
+            return searchWordRecursive(mot, index + 1, indexPath, i - 1, j).Concat(
+                searchWordRecursive(mot, index + 1, indexPath, i, j - 1)).ToList().Concat(
+                searchWordRecursive(mot, index + 1, indexPath, i, j + 1)).ToList().Concat(
+                searchWordRecursive(mot, index + 1, indexPath, i - 1, j - 1)).ToList().Concat(
+                searchWordRecursive(mot, index + 1, indexPath, i - 1, j + 1)).ToList();
+        } else {
+            indexPath.Add(new int[2] {i,j});
+            return indexPath;
+        }
+    }
+
+    public void updateBoard(List<int[]> indexes) {
+        foreach(int[] indexPair in indexes) {
+            tableau[indexPair[0],indexPair[1]] = null;
+        }
+        for (int i = 0; i < tableau.GetLength(0); i++) {
+            for (int j = 1; j < tableau.GetLength(1); j++) {
+                if (tableau[i,j] == null) {
+                    int lastIndex = -1;
+                    for (int k = i; k >= 1 && tableau[k-1,j] != null; k--) {
+                        tableau[k,j] = tableau[k-1,j];
+                        lastIndex = k;
+                    }
+                    tableau[lastIndex-1,j] = null;
+                }
+            }
+        }
+    }
 }
--- Program.cs
+++ Program.cs
@@ -4,18 +4,25 @@ class Program
 {
     static void Main()
     {
-        string filePath="Mots_Français.txt";
-        string langage="Français";
+        // string filePath="Mots_Français.txt";
+        // string langage="Français";
 
-        Dictionnaire dico=new Dictionnaire(langage, filePath);
-        string s=dico.toString();
-        Console.WriteLine(s);
+        // Dictionnaire dico=new Dictionnaire(langage, filePath);
+        // string s=dico.toString();
+        // Console.WriteLine(s);
 
-        string motrecherché="tennis";
-        bool b=dico.RechDichoRecursif(motrecherché);
-        if (b)
-            Console.WriteLine("Le mot "+motrecherché+" a été trouvé dans le dictionnaire.");
-        else
-            Console.WriteLine("Le mot "+motrecherché+" n'a pas été trouvé dans le dictionnaire.");
+        // string motrecherché="tennis";
+        // bool b=dico.RechDichoRecursif(motrecherché);
+        // if (b)
+        //     Console.WriteLine("Le mot "+motrecherché+" a été trouvé dans le dictionnaire.");
+        // else
+        //     Console.WriteLine("Le mot "+motrecherché+" n'a pas été trouvé dans le dictionnaire.");
+
+        Lettre[,] letterBoard = Plateau.fetchBoardFromFile("Test1.csv");
+        Plateau board = new Plateau(letterBoard);
+        Console.WriteLine(board.toString());
+        Joueur joueur = new Joueur("Clement");
+        board.updateBoard(board.searchWord(Console.ReadLine()!, joueur));
+        Console.WriteLine(board.toString());
     }
 }
