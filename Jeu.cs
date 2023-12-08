e1538cc5e0258af0c487a857a43d394b23f611c4
Clément <clement.garrigou@edu.devinci.fr>
2023-12-08 21:58:34 +0100

    avancement classe Jeu

--- Jeu.cs
+++ Jeu.cs
@@ -1,6 +1,16 @@
 namespace wordCrush {
 public class Jeu {
+    readonly Dictionnaire dictionnaire;
+    Plateau board;
+    Joueur[] joueurs;
+
+    public Jeu(Dictionnaire dictionnaire, Plateau board, Joueur[] joueurs) {
+        this.dictionnaire = dictionnaire;
+        this.board = board;
+        this.joueurs = joueurs;
+    }
 
     
+
 }
 }
