ab49cc1e70e4a38c78459493ab88f469c7598a81
Clément <clement.garrigou@edu.devinci.fr>
2023-12-08 22:04:04 +0100

    use File class to read from csv

--- csvInterface.cs
+++ csvInterface.cs
@@ -18,21 +18,13 @@ public class csvInterface {
     /// </summary>
     /// <returns>Returns parsed data as double array of char using given filename and separator</returns>
     public string[,] parseFromFile() {
-        StreamReader? streamReader = null;
         int counter = 0;
-        string? nextLine;
         string[,] lines = new string[0,0]; //dummy value, reading length from file right after this
-        List<string> linesList = new List<string>();
         try {
-            streamReader = new StreamReader(filename);
-            while ((nextLine = streamReader.ReadLine()) != null) {
-                linesList.Add(nextLine);
-                
-            }
-            int nbrLines = linesList.Count;
-            lines = new string[nbrLines,linesList[0].Split(separator).Length];
-            while (counter < nbrLines) {
-                string[] broke = linesList[counter].Split(separator);
+            string[] readLines = File.ReadAllLines(filename);
+            lines = new string[readLines.Length,readLines[0].Split(separator).Length];
+            while (counter < readLines.Length) {
+                string[] broke = readLines[counter].Split(separator);
                 for (int i = 0; i < broke.Length; i++) {
                     lines[counter,i] = broke[i];
                 }
@@ -43,8 +35,7 @@ public class csvInterface {
         } catch (Exception e) {
             Console.WriteLine(e.Message);
         }
-        
-        streamReader?.Close(); //not using finally block because return statement not allowed inside finally
+         //not using finally block because return statement not allowed inside finally
         return lines;
     }
 }
